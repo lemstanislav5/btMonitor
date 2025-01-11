6bb4121ea5df972c1265ee64e240d56c05ce9042
lemstanislav5 <lemstanislav5@gmail.com>
2025-01-11 14:07:09 +0300

    add json and hex

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation(libs.eventbus)
-    implementation (libs.otto)
+    implementation (libs.gson)
 }
--- app/src/main/java/com/example/btapp/bluetooth/ReceiveThread.java
+++ app/src/main/java/com/example/btapp/bluetooth/ReceiveThread.java
@@ -5,6 +5,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -45,11 +48,24 @@ public class ReceiveThread extends Thread{
                         key = newKey;
                         Log.d("MyLog", "class ReceiveThread key: " + key);
 
+                        // Парсим json стороку в массив int[]
+                        GsonBuilder builder = new GsonBuilder();
+                        Gson gson = builder.create();
+
+                        // Приводим массив к строке с данными HEX
+                        StringBuilder keyToHex = new StringBuilder();
+                        int[] array = gson.fromJson(key, int[].class);
+                        for (int i = 0; i < array. length; i++){
+                            keyToHex.append(Integer.toHexString(array[i])).append(":");
+                        }
+
+                        // Отправляем строку в приемник
                         Intent intent = new Intent();
-                        intent.putExtra("com.example.snippets.DATA", key);
+                        intent.putExtra("com.example.snippets.DATA", String.valueOf(keyToHex));
                         intent.setPackage("com.example.btapp");
                         intent.setAction("MY_ACTION");
                         context.sendBroadcast(intent);
+
                     }
                 }
             } catch (IOException err){
@@ -64,6 +80,33 @@ public class ReceiveThread extends Thread{
             outputStream.write(byteArray);
         } catch (IOException err){
             Log.d("MyLog", "Error sendMessage: " + String.valueOf(err));
+        }
+    }
+    class Test {
+        private static final int sizeOfIntInHalfBytes = 8;
+        private static final int numberOfBitsInAHalfByte = 4;
+        private static final int halfByte = 0x0F;
+        private  final char[] hexDigits = {
+                '0', '1', '2', '3', '4', '5', '6', '7',
+                '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
+        };
+
+        public  String decToHex(int dec) {
+            StringBuilder hexBuilder = new StringBuilder(sizeOfIntInHalfBytes);
+            hexBuilder.setLength(sizeOfIntInHalfBytes);
+            for (int i = sizeOfIntInHalfBytes - 1; i >= 0; --i)
+            {
+                int j = dec & halfByte;
+                hexBuilder.setCharAt(i, hexDigits[j]);
+                dec >>= numberOfBitsInAHalfByte;
+            }
+            return hexBuilder.toString();
+        }
+
+        public  void main(String[] args) {
+            int dec = 305445566;
+            String hex = decToHex(dec);
+            System.out.println(hex);
         }
     }
 }
